8061fec7cc6539e0edb2576b99f932ca3e750825
alirezayoom <alirezayoom@gmail.com>
2023-06-28 10:19:06 +0430

    add portal

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,56 +1,62 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Backdrop = ({ onConfirm }) => (
-  <div
-    onClick={onConfirm}
-    style={{
-      backgroundColor: "#222b",
-      left: 0,
-      top: 0,
-      position: "absolute",
-      width: "100vw",
-      height: "100vh",
-      display: "grid",
-      placeContent: "center",
-      zIndex: 99,
-    }}
-  ></div>
-);
-
-const ModalOverlay = ({ onConfirm }) => {
-  <div
-    style={{
-      position: "absolute",
-      left: "50%",
-      top: "50%",
-      transform: "translate(-50%, -50%)",
-      width: "300px",
-      display: "block",
-      backgroundColor: "#333",
-      zIndex: 100,
-    }}
-  >
-    <div style={{ padding: "1rem" }}>{title}</div>
-    <div style={{ height: "150px", padding: "1rem", backgroundColor: "#444" }}>
-      {message}
-    </div>
+const Backdrop = ({ onConfirm }) => {
+  return (
     <div
       onClick={onConfirm}
       style={{
-        padding: "1rem",
-        cursor: "pointer",
-        textAlign: "center",
+        backgroundColor: "#222b",
+        left: 0,
+        top: 0,
+        position: "absolute",
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        placeContent: "center",
+        zIndex: 99,
+      }}
+    ></div>
+  );
+};
+
+const ModalOverlay = ({ title, message, onConfirm }) => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+        transform: "translate(-50%, -50%)",
+        width: "300px",
+        display: "block",
+        backgroundColor: "#333",
+        zIndex: 100,
       }}
     >
-      Close
+      <div style={{ padding: "1rem" }}>{title}</div>
+      <div
+        style={{ height: "150px", padding: "1rem", backgroundColor: "#444" }}
+      >
+        {message}
+      </div>
+      <div
+        onClick={onConfirm}
+        style={{
+          padding: "1rem",
+          cursor: "pointer",
+          textAlign: "center",
+        }}
+      >
+        Close
+      </div>
     </div>
-  </div>;
+  );
 };
 
 const Error = ({ title, message, onConfirm }) => {
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       {ReactDOM.createPortal(
         <Backdrop onConfirm={onConfirm} />,
         document.getElementById("backdrop-root")
